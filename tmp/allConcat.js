f92d76b6aca368a2034a050da2b65d5415a18da1
matthew newell <mnewell10@gmail.com>
2016-03-18 13:00:23 -0700

    Remember that I need more than one file

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -42,3 +42,15 @@ $(document).ready(function() {
   });
 });
 
+// var getAllRepos = require('./../js/browser-interface.js').getAllRepos;
+//
+// $(document).ready(function(){
+//   $("#button").click(function(e) {
+//     e.preventDefault();
+//     getAllRepos($("#userName").val());
+//   });
+// });
+
+
+//I need to split up my ui logic to have functions/methods in multile files so that it is more readible ? spelling
+// break time
